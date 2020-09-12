4539add0c17d60aee3a02ce3082ea5192430575a
no1porkbun <anthonygaskin95@gmail.com>
2020-09-12 20:33:03 +1200

    Transportation Method

--- src/pack1/BIT504AS2_PartD/GitHub_Application.java
+++ src/pack1/BIT504AS2_PartD/GitHub_Application.java
@@ -5,6 +5,7 @@ public class GitHub_Application {
 	public static void main(String[] args) {
 		System.out.print("Hi, my name is Anthony");
 		System.out.print("My favourite colour is Blue");
+		System.out.print("My favourite mode of transportation is walking");
 	}
 
 }

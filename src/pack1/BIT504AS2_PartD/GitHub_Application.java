5e17e5578abbd431ae2951d3b700da1bd1d28cdc
no1porkbun <anthonygaskin95@gmail.com>
2020-09-12 20:34:19 +1200

    Day of the Week

--- src/pack1/BIT504AS2_PartD/GitHub_Application.java
+++ src/pack1/BIT504AS2_PartD/GitHub_Application.java
@@ -6,6 +6,7 @@ public class GitHub_Application {
 		System.out.print("Hi, my name is Anthony");
 		System.out.print("My favourite colour is Blue");
 		System.out.print("My favourite mode of transportation is walking");
+		System.out.print("The best day of the week is Friday");
 	}
 
 }
